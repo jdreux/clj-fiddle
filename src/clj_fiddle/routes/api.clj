24d1875c6174dd949936be460224484fe8355e3d
Julien Dreux <julien.dreux@gmail.com>
2013-11-27 00:33:13 -0500

    layout work

--- src/clj_fiddle/routes/api.clj
+++ src/clj_fiddle/routes/api.clj
@@ -7,42 +7,16 @@
   (:import java.io.StringWriter
            java.util.concurrent.TimeoutException))
 
-
-
-(defn eval-form [form sbox]
-  (with-open [out (StringWriter.)]
-    (let [result (sbox form {#'*out* out})]
-      {:expr form
-       :result [out result]})))
-
-(defn eval-string [expr sbox]
-  (println "evaling: " expr)
-  (let [form (binding [*read-eval* false] (read-string expr))]
-    (eval-form form sbox)))
-
-(def try-clojure-tester
-  (conj secure-tester-without-def (blanket "tryclojure" "noir")))
-
-(defn make-sandbox []
-  (sandbox try-clojure-tester
-           :timeout 2000
-           :init '(do (require '[clojure.repl :refer [doc source]])
-                      (future (Thread/sleep 600000)
-                              (-> *ns* .getName remove-ns)))))
-
 (defroutes api-routes
   (GET "/" [] (json {:ok true}))
   (POST "/eval" [code] (json (with-open [out (StringWriter.)]
-                               (println "Evaling" code)
                                (try
-                                 (let [sbox (sandbox secure-tester-without-def)
+                                 (let [sbox (sandbox secure-tester-without-def :timeout 2000)
                                        form (binding [*read-eval* false] (read-string code))
                                        result (sbox form {#'*out* out})
                                        response {:result (pr-str result) :stdout (.toString out)}]
-                                   (println "got results" response)
                                    response)
                                    (catch TimeoutException _
                                      {:error true :message "Execution Timed Out!"})
                                    (catch Exception e
-                                     (println e)
                                      {:error true :message (str (root-cause e))}))))))
